d45d7c5ead157193ac787a2631bf468da13a4367
maro911220 <kingdomboze@gmail.com>
2023-08-11 22:42:11 +0900

    detail page edit

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -2,7 +2,6 @@
 import useStore from "@/store/store";
 import { motion } from "framer-motion";
 import { useEffect } from "react";
-import { AiOutlineLink } from "react-icons/ai";
 import Loadings from "../../loadings";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -23,6 +22,8 @@ const slideStyle =
   "border rounded bg-white overflow-hidden dark:border-neutral-700 dark:bg-neutral-800";
 const slideTextStyle = "text-sm p-1 h-12 text-ellipsis overflow-hidden";
 const slideImgStyle = "w-full h-20 object-cover";
+const moreBtnStyle =
+  "w-full p-4 flex items-center justify-center rounded transition bg-neutral-100 border border-neutral-200 hover:bg-neutral-200 dark:border-neutral-700 dark:bg-neutral-600 dark:hover:bg-neutral-700";
 
 /* Detail */
 export default function Detail(props) {
@@ -107,8 +108,33 @@ export default function Detail(props) {
           {/* Synopsis */}
           <div className={boxStyle}>
             <h3 className={`${titleStyle} mb-2`}>Synopsis</h3>
-            <p className={textStyle}>{dataCheck(item.items[0].synopsis)}</p>
+            {item.items[0].synopsis ? (
+              <p className={textStyle}>{dataCheck(item.items[0].synopsis)}</p>
+            ) : (
+              <p className={textStyle}>not data</p>
+            )}
           </div>
+          {/* Trailer */}
+          <div className={boxStyle}>
+            <h3 className={`${titleStyle} mb-2`}>Trailer</h3>
+            {item.items[0].trailer.youtube_id ? (
+              <iframe
+                className="w-full rounded h-[45vw] max-h-[800px]"
+                src={`https://www.youtube.com/embed/${item.items[0].trailer.youtube_id}`}
+                title={item.items[0].title}
+              ></iframe>
+            ) : (
+              <p className={textStyle}>not data</p>
+            )}
+          </div>
+
+          <a
+            className={moreBtnStyle}
+            href={dataCheck(item.items[0].url)}
+            target="_blank"
+          >
+            More Info
+          </a>
         </motion.div>
       ) : (
         <Loadings />
